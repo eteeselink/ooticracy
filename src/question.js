974910d5d5d7c93ec1ed085c04cd95cc9c9c54ce
Vladimir Pchelin <vsemp@bu.edu>
2017-11-20 15:54:18 +0100

    Integrated questionaire

--- src/question.js
+++ src/question.js
@@ -4,6 +4,7 @@ export class Question{
      */
     constructor(element) {
         this.element = element;
+        this.record = [];
     }
 
     
@@ -11,6 +12,7 @@ export class Question{
     
 
     render_question(array){
+        var button_id;
         var rendering =  `
         question:   ${array[0]} <br>
        
@@ -18,16 +20,44 @@ export class Question{
         for (var i = 1; i<array.length;i++){
         
         var string = `
-        <input type="radio" name=${array[i]} value=${array[i]} id="radio1">
+        <input type="radio" name="question" value=${array[i]} id=${i}>
         <label for="radio1">${array[i]}</label><br>
             `
         
         rendering = rendering.concat( string);   
-            
-        }
+        
+        
 
+        }
+        var button_string = `<button id="btn" class="yes">Vote!</button>`;
+
+        rendering = rendering.concat( button_string); 
 
         this.element.innerHTML = rendering;
+
+        var sbutton = document.createElement("button");
+        sbutton.innerHTML = "Submit";
+
+        this.element.appendChild(sbutton);
+
+        sbutton.addEventListener ("click", () => {
+            alert("Number of answers is " + this.record.length.toString());
+        });
+
+        this.element.querySelector("#btn").addEventListener("click", ev => {
+            // always add `preventDefault` in an event handler. otherwise, the browser
+            // will do some default action which usually means submitting the data to the server, 
+            // which causes the entire page to reload.
+            // since we have no server, we don't want that :-)
+            ev.preventDefault();
+            
+            var bestPizza = this.element.querySelector("input[name=question]:checked").value;
+            this.record.push([array[0],bestPizza]);
+            alert("Your answer was " + this.record[this.record.length - 1]);
+            // this.element.innerHTML = `<p>Indeed ${name}, Pizza ${bestPizza} is by far the best.</p><div id="pizza"></div>`;
+            
+            // makeAsciiArt(this.element.querySelector("#pizza"));
+        })
 
     }
 
